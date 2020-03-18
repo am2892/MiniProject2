bbcfe02000c3143601b5eb43e3486d77b46f9b54
ArianaRuggiero <60370127+ArianaRuggiero@users.noreply.github.com>
2020-03-18 17:27:43 -0400

    FEATURE: added Population Mean, Variance of Population Proportion, Confidence Interval, and Sample Mean functions

--- StatisticalCalculator.py
+++ StatisticalCalculator.py
@@ -15,24 +15,40 @@ dataSet = readCSV('CSV_files/test.csv')
 choice = ''
 
 print('Select operation!')
-print('1.Median')
-print('2.Standard Deviation')
-print('3.Calculate Z-Score for each Number')
-print('4.Calculate Population Variance')
+print('1.Population Mean')
+print('2.Median')
+print('5.Variance of Population Proportion')
+print('6.Calculate Z-Score for each Number')
+print('9.Confidence Interval')
+print('10.Calculate Population Variance')
+print('13.Sample Mean')
+print('14.Standard Deviation')
 
 choice = input('Enter choice:')
 
 if choice == "1":
+    from PopulationMean import populationMean
+    print('population mean: ', str(populationMean(dataSet)))
+elif choice == "2":
     from Median import median
     print('median: ', str(median(dataSet)))
-elif choice == "2":
-    from StandardDeviation import standardDeviation
-    print('standard deviation: ', str(standardDeviation(dataSet)))
-elif choice == "3":
+elif choice == "5":
+    from VariancePopulationProportion import variancePopulationProportion
+    print('variance of population proportion: ', str(variancePopulationProportion(dataSet)))    
+elif choice == "6":
     from zScore import zScore
     print('z-score: ', str(zScore(dataSet)))
-elif choice == "4":
+elif choice == "9":
+    from ConfidenceInterval import confidenceInterval
+    print('confidence interval: ', str(confidenceInterval(dataSet)))    
+elif choice == "10":
     from Variance import variance
     print('variance: ', str(variance(dataSet)))
+elif choice == "13":
+    from SampleMean import sampleMean
+    print('sample mean: ', str(sampleMean(dataSet)))    
+elif choice == "14":
+    from StandardDeviation import standardDeviation
+    print('standard deviation: ', str(standardDeviation(dataSet)))    
 else:
     print("(•̀ᴗ•́ )(•̀ᴗ•́ )(•̀ᴗ•́ ) something went wrong (•̀ᴗ•́ )(•̀ᴗ•́ )(•̀ᴗ•́ )")
