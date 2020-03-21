59181b5d41f45f770a47c15289ad863f4a43308e
Sherif Elmetwally <sge5@njit.edu>
2020-03-21 14:00:59 -0400

    FEATURE: create Statistics module and update README.md

---
+++ StatisticsModule.py
@@ -0,0 +1,37 @@
+# import our readCSV file so we can read the csv
+from FileReader import readCSV
+import math
+
+dataSet = readCSV('CSV_files/test.csv')
+
+#1 Population Mean
+
+#2 Median
+
+#3 Mode 
+
+#4 Population Variance
+
+#5 Variance of the population proportion
+
+#6 Z-Score
+
+#7 Standardized Score 
+
+#8 Population Correlation Coefficient 
+
+#9 Confidence Interval
+
+#10 Population Variance
+
+#11 P Value
+
+#12 Proportion
+
+#13 Sample Mean
+
+#14 Sample Standard Deviation
+
+#15 Variance of sample proportion
+
+
